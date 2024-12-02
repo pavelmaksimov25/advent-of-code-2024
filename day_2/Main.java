f34882edd1cd74ff7882e6b86d0416a9a733b9ac
Pavlo Maksymov <maksimovpb@gmail.com>
2024-12-02 10:38:04 +0100

    chore: day 2 structure

---
+++ day_2/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+	static void solutionOne() {
+	}
+
+	static void solutionTwo() {
+	}
+
+    public static void main(String[] args) {
+    	solutionOne();
+		solutionTwo();
+    }
+
+	public static String getTestInput() {
+		return "";
+	}
+
+
+	public static String getInput() {
+		return "";
+	}
+}
