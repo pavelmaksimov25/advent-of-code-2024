cf5de4488d29a87a65f11b721237982833b18db7
Pavlo Maksymov <maksimovpb@gmail.com>
2024-12-04 15:21:22 +0100

    day3: introduced new folder and renamed folders

---
+++ DayThree/MainDayThree.java
@@ -0,0 +1,25 @@
+public class MainDayThree {
+    static void solutionOne() {
+        String input = getTestInput();
+
+        System.out.println(input);
+        System.out.println("Expected result: 161");
+    }
+
+    static void solutionTwo() {
+    }
+
+    public static void main(String[] args) {
+        solutionOne();
+//        solutionTwo();
+    }
+
+    public static String getTestInput() {
+        return "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))\n";
+    }
+
+
+    public static String getInput() {
+        return "";
+    }
+}
