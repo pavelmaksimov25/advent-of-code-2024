ceb8a7b0fb914d3c8124a897cbe4efa85eab9560
Pavlo Maksymov <maksimovpb@gmail.com>
2024-12-05 16:38:25 +0100

    daye(round1): success on test data

--- src/DayFour/MainDayFour.java
+++ src/DayFour/MainDayFour.java
@@ -26,18 +26,92 @@ public class MainDayFour {
     }
 
     private static int findXmas(int i, int j, String[] lines) {
-        // horizontal left to right
+        int score = 0;
         try {
+            // horizontal left to right
             if (lines[i].charAt(j) == 'X' && lines[i].charAt(j + 1) == 'M' && lines[i].charAt(j + 2) == 'A' && lines[i].charAt(j + 3) == 'S') {
-                System.out.println("XMAS");
+                System.out.println("horizontal: XMAS");
 
-                return 1;
+                score += 1;
             }
         } catch (Exception e) {
-            System.out.println("fail");
         }
 
-        return 0;
+        try {
+            // horizontal backwards
+            if (lines[i].charAt(j) == 'X' && lines[i].charAt(j - 1) == 'M' && lines[i].charAt(j - 2) == 'A' && lines[i].charAt(j - 3) == 'S') {
+                System.out.println("horizontal backwards: XMAS");
+
+                score += 1;
+            }
+        } catch (Exception e) {
+        }
+
+        try {
+            // vertical
+            if (lines[i].charAt(j) == 'X' && lines[i + 1].charAt(j) == 'M' && lines[i + 2].charAt(j) == 'A' && lines[i + 3].charAt(j) == 'S') {
+                System.out.println("vertical: XMAS");
+
+                score += 1;
+
+            }
+        } catch (Exception e) {
+        }
+        try {
+            // vertical backwards
+            if (lines[i].charAt(j) == 'X' && lines[i - 1].charAt(j) == 'M' && lines[i - 2].charAt(j) == 'A' && lines[i - 3].charAt(j) == 'S') {
+                System.out.println("vertical upside down: XMAS");
+
+                score += 1;
+
+            }
+        } catch (Exception e) {
+        }
+        try {
+            // diagonal top-left to bottom-right
+            if (lines[i].charAt(j) == 'X' && lines[i + 1].charAt(j + 1) == 'M' && lines[i + 2].charAt(j + 2) == 'A' && lines[i + 3].charAt(j + 3) == 'S') {
+                System.out.println("diagonal top-left to bottom-right: XMAS");
+
+                score += 1;
+
+            }
+        } catch (Exception e) {
+        }
+
+        try {
+            // diagonal top-right to bottom-left
+            if (lines[i].charAt(j) == 'X' && lines[i + 1].charAt(j - 1) == 'M' && lines[i + 2].charAt(j - 2) == 'A' && lines[i + 3].charAt(j - 3) == 'S') {
+                System.out.println("diagonal top-right to bottom-left: XMAS");
+
+                score += 1;
+
+            }
+        } catch (Exception e) {
+        }
+
+        try {
+            // diagonal bottom-left to top-right
+            if (lines[i].charAt(j) == 'X' && lines[i - 1].charAt(j + 1) == 'M' && lines[i - 2].charAt(j + 2) == 'A' && lines[i - 3].charAt(j + 3) == 'S') {
+                System.out.println("diagonal bottom-left to top-right: XMAS");
+
+                score += 1;
+
+            }
+        } catch (Exception e) {
+        }
+
+        try {
+            // diagonal bottom-right to top-left
+            if (lines[i].charAt(j) == 'X' && lines[i - 1].charAt(j - 1) == 'M' && lines[i - 2].charAt(j - 2) == 'A' && lines[i - 3].charAt(j - 3) == 'S') {
+                System.out.println("diagonal bottom-right to top-left: XMAS");
+
+                score += 1;
+
+            }
+        } catch (Exception e) {
+        }
+
+        return score;
     }
 
     public static void main(String[] args) {
