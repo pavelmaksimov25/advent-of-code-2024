ed819e1385413ce5efb7af1a485c2b8ffb739201
Pavlo Maksymov <maksimovpb@gmail.com>
2024-12-08 14:28:37 +0100

    fay5(round1): init

---
+++ src/DayFive/MainDayOne.java
@@ -0,0 +1,26 @@
+package DayFive;
+
+import java.util.*;
+
+public class MainDayOne {
+	static void roundOne() {
+
+	}
+
+	static void roundTwo() {
+	}
+
+    public static void main(String[] args) {
+    	roundOne();
+//		roundTwo();
+    }
+
+	public static String getTestInput() {
+		return "";
+	}
+
+
+	public static String getInput() {
+		return "";
+	}
+}
